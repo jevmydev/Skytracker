64df5dc067ba70723b66c1cc3482636487a2fb80
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-26 18:19:15 -0300

    Add location and transform to ms constants

--- src/utils/const.js
+++ src/utils/const.js
@@ -3,9 +3,13 @@ export const DEFAULT_POSITION = {
     lon: 10
 };
 
+export const TRANSFORM_TO_MS = 1000;
+
 export const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 export const WEATHER_URL_ICON = "http://openweathermap.org/img/wn/";
 
 export const FORECASTS_URL = "https://api.openweathermap.org/data/2.5/forecast";
+
+export const LOCATION_URL = "http://api.openweathermap.org/geo/1.0/direct";
 
 export const WEATHERMAPS_URL = "https://tile.openweathermap.org/map/clouds_new/20";
