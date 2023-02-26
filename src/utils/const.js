7d9432e7cf74254823377c620d5f64649f990138
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-26 13:38:29 -0300

    Update const of API

--- src/utils/const.js
+++ src/utils/const.js
@@ -6,4 +6,6 @@ export const DEFAULT_POSITION = {
 export const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 export const WEATHER_URL_ICON = "http://openweathermap.org/img/wn/";
 
+export const FORECASTS_URL = "https://api.openweathermap.org/data/2.5/forecast";
+
 export const WEATHERMAPS_URL = "https://tile.openweathermap.org/map/clouds_new/20";
