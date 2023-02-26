10aa855d6cc3b7b4753143df49ff7e9bd295ae4b
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-26 14:03:23 -0300

    Update loading

--- src/elements/Loading.jsx
+++ src/elements/Loading.jsx
@@ -11,6 +11,9 @@ export function Loading() {
             <div className="flex w-full">
                 <div className="bg-zinc-800 w-full h-72 rounded"></div>
             </div>
+            <div className="flex w-full">
+                <div className="bg-zinc-800 w-full h-36 rounded"></div>
+            </div>
         </div>
     );
 }
