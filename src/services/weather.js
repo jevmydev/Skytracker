53be69c129fd81db901a7e3b4552304c09155942
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-26 18:20:04 -0300

    Change date compromise

--- src/services/weather.js
+++ src/services/weather.js
@@ -1,24 +1,25 @@
-import { WEATHER_URL, WEATHER_URL_ICON } from "../utils/const";
+import { WEATHER_URL, WEATHER_URL_ICON, TRANSFORM_TO_MS } from "../utils/const";
 import { formatTemp, formatPercentage, formatSplitComma, capitalize, formatDate } from "../utils/formats";
 
 function compromiseWeather({ resWeather }) {
     return resWeather?.map((dataWeather) => {
-        const { weather, main, sys, name } = dataWeather;
-        const weatherFirst = weather[0];
+        const { weather, main, sys, name, dt } = dataWeather;
+        const weatherFirst = weather ? weather[0] : {};
 
         return {
             place: name,
-            country: sys.country,
-            icon: `${WEATHER_URL_ICON}${weatherFirst.icon}.png`,
-            description: capitalize({ text: weatherFirst.description }),
-            fullPlace: formatSplitComma({ text: `${name} ${sys.country}` }),
-            temp: formatTemp({ temp: main.temp }),
-            maxTemp: formatTemp({ temp: main.temp_max }),
-            minTemp: formatTemp({ temp: main.temp_min }),
-            likeTemp: formatTemp({ temp: main.feels_like }),
-            humidity: formatPercentage({ value: main.humidity }),
-            sunrise: formatDate(new Date(sys.sunrise))[1],
-            sunset: formatDate(new Date(sys.sunset))[1]
+            fullPlace: formatSplitComma({ text: `${name} ${sys?.country}` }),
+            country: sys?.country,
+            icon: `${WEATHER_URL_ICON}${weatherFirst?.icon}.png`,
+            date: formatDate({ date: new Date(dt * TRANSFORM_TO_MS) }),
+            description: capitalize({ text: weatherFirst?.description }),
+            temp: formatTemp({ temp: main?.temp }),
+            maxTemp: formatTemp({ temp: main?.temp_max }),
+            minTemp: formatTemp({ temp: main?.temp_min }),
+            likeTemp: formatTemp({ temp: main?.feels_like }),
+            humidity: formatPercentage({ value: main?.humidity }),
+            sunrise: formatDate({ date: new Date(sys.sunrise * TRANSFORM_TO_MS) })[1],
+            sunset: formatDate({ date: new Date(sys.sunset * TRANSFORM_TO_MS) })[1]
         };
     })[0];
 }
