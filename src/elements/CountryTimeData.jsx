c4560748176e59e0d7134611192df1be6c008f2b
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-26 13:40:13 -0300

    Update case of not found data

--- src/elements/CountryTimeData.jsx
+++ src/elements/CountryTimeData.jsx
@@ -3,7 +3,7 @@ export function CountryTimeData({ data, icon: Icon, image, ...props }) {
         <h3 className="flex items-center gap-1" {...props}>
             {Icon && <Icon stroke="stroke-red-200" />}
             {image && <img src={image} alt={`Imagen del clima actual sobre ${data}`} />}
-            {data}
+            {data && data}
         </h3>
     );
 }
