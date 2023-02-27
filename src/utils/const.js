164bc93babff9b5a695ae160752cc127436927e8
Jeremy Díaz <jeremy664k@gmail.com>
2023-02-27 11:46:15 -0300

    Add debounce wait and localdate consts & update default

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,15 +1,18 @@
 export const DEFAULT_POSITION = {
-    lat: 10,
-    lon: 10
+    lat: -33.45694,
+    lon: -70.64827
 };
+
+export const DEBOUNCE_WAIT_TIME_SEARCH = 300;
 
 export const TRANSFORM_TO_MS = 1000;
 
 export const WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
 export const WEATHER_URL_ICON = "https://openweathermap.org/img/wn/";
+export const WEATHERMAPS_URL = "https://tile.openweathermap.org/map/clouds_new/20";
 
 export const FORECASTS_URL = "https://api.openweathermap.org/data/2.5/forecast";
 
 export const LOCATION_URL = "https://api.openweathermap.org/geo/1.0/direct";
 
-export const WEATHERMAPS_URL = "https://tile.openweathermap.org/map/clouds_new/20";
+export const LOCALDATE_URL = "https://api.timezonedb.com/v2.1/get-time-zone";
